0205f100551658be362888921cccb98be43fa270
Mehmet Can Hanaylı <mehmet@parskod.com>
2024-06-03 22:50:02 +0300

    Todo Projesi baslatıldı ve backend parçalama işlemi yapıldı.

---
+++ app-2/application/views/includes/footer.php
@@ -0,0 +1,6 @@
+<!-- To the right -->
+<div class="float-right d-none d-sm-inline">
+    Anything you want
+</div>
+<!-- Default to the left -->
+<strong>Copyright &copy; <?php echo date("Y"); ?> <a href="https://parskod.com">ParsKOD</a>.</strong> All rights reserved.
